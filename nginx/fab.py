3124b5e61c38788511108243aab182e98f1b57d6
brendan <mail.brendan.kavanagh@gmail.com>
2015-02-03 12:04:17 +0000

    added NGINX

--- nginx/fab.py
+++ nginx/fab.py
@@ -6,6 +6,9 @@ config_dir = os.path.join(os.path.dirname(__file__), 'sites-enabled')
 def enable_webapp_config():
     with lcd(config_dir):
         local('cp * /etc/nginx/sites-enabled')
+        local('cp * /etc/nginx/sites-available')
+    local('rm -f /etc/nginx/sites-enabled/default')
+    local('rm -f /etc/nginx/sites-available/default')
 
 if __name__ == '__main__':
     enable_webapp_config()
